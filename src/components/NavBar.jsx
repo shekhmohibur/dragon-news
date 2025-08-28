5c724f8f6d2db2be35b69af0eed3101732db6989
shekhmohibur <mohibur.rahman2003@gmail.com>
2025-08-29 00:26:23 +0600

    header section design done and main section layout okay

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import userIcon from '../assets/user.png'
+const NavBar = () => {
+    return (
+        <div className="flex flex-col md:flex-row justify-between items-center py-5">
+            <div className=""></div>
+            <div className="space-x-6 *:text-[#706F6F] *:md:text-lg">
+                <Link to={'/'}>Home</Link>
+                <Link to={'/about'}>About</Link>
+                <Link to={'/career'}>Career</Link>
+            </div>
+            <div className="login flex gap-2">
+                <img src={userIcon} alt="userIcon" />
+                <button className="btn bg-[#403F3F] btn-neutral rounded-none font-semibold px-8 text-xl">Login</button>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,17 +1,20 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import NavBar from "../components/NavBar";
 
 const HomeLayout = () => {
     return (
-        <div className="font-poppins">
+        <div className="font-poppins w-11/12 mx-auto">
             <header>
                 <Header></Header>
-                 <section className="mx-auto max-w-6xl">
-                    <LatestNews></LatestNews>
-                 </section>
+                <LatestNews></LatestNews>
+                <NavBar></NavBar>
             </header>
-            <nav></nav>
-            <main></main>
+            <main className="grid grid-cols-12">
+                <aside className="col-span-3">Left Nav</aside>
+                <section className="col-span-6">Main Content</section>
+                <aside className="col-span-3">Right Nav</aside>
+            </main>
         </div>
     );
 };
