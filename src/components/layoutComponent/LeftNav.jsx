af0cf6507f9d1a766e5e5895dd9f1ccf1a1dee21
shekhmohibur <mohibur.rahman2003@gmail.com>
2025-09-01 22:20:08 +0600

    loaded news data in homvecomponent and showing it as card

--- src/components/layoutComponent/LeftNav.jsx
+++ src/components/layoutComponent/LeftNav.jsx
@@ -13,7 +13,7 @@ const LeftNav = () => {
             <h2 className="font-semibold md:text-xl">All Category</h2>
             <div className="flex flex-col gap-2 mt-4">
                 {
-                    categories?.map(category => <NavLink className="btn font-semibold md:text-xl max-w-64 py-6 bg-base-100 border-none rounded-none hover:bg-[#E7E7E7]" key={category.category_id}>{category.category_name}</NavLink>)
+                    categories?.map(category => <NavLink className="btn font-semibold md:text-xl max-w-64 py-6 bg-base-100 border-none rounded-none hover:bg-[#E7E7E7]" to={`/category/${category.category_id}`} key={category.category_id}>{category.category_name}</NavLink>)
                 }
             </div>
         </div>
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
 import LeftNav from "../components/layoutComponent/LeftNav";
@@ -16,7 +17,9 @@ const HomeLayout = () => {
                 <aside className="col-span-3">
                     <LeftNav></LeftNav>
                 </aside>
-                <section className="col-span-6">Main Content</section>
+                <section className="col-span-6">
+                    <Outlet></Outlet>
+                </section>
                 <aside className="col-span-3">
                     <RightNav></RightNav>
                 </aside>
